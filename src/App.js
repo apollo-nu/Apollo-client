6e96d4651b544b0212059bc4b4ba500fcfedc061
Cooper Barth <cooper.barth@getbraintree.com>
2019-07-15 09:58:50 -0500

    Can move between rows

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import {DragDropContext} from "react-beautiful-dnd";
-
+import "./css/App.css";
 import Row from "./components/Row";
 
 const initialData = {
@@ -11,6 +11,13 @@ const initialData = {
       { id: 'one', content: '1' },
       { id: 'three', content: '3' },
       { id: 'two', content: '2' }
+    ],
+    'column-2': [
+      { id: 'six', content: '6' },
+      { id: 'seven', content: '7' },
+      { id: 'eight', content: '8' },
+      { id: 'nine', content: '9' },
+      { id: 'ten', content: '10' }
     ]
   }
 };
@@ -23,50 +30,39 @@ class App extends Component {
   }
 
   onDragEnd(result) {
-    const { destination, source, draggableId } = result;
+    const { destination, source } = result;
     if (!(destination && source)) { return }
-    console.log(destination, source);
 
     const columns = this.state.columns;
     let sourceColumn = columns[source.droppableId];
-    let destColumn = columns[source.droppableId];
+    let destColumn = columns[destination.droppableId];
 
-    const item = sourceColumn.splice(source.index, 1);
-    console.log(item);
+    const item = sourceColumn.splice(source.index, 1)[0];
     destColumn.splice(destination.index, 0, item);
+
     columns[source.droppableId] = sourceColumn;
     columns[destination.droppableId] = destColumn;
-
-    this.setState({
-      columns: columns
-    })
-  
-    /*
-    const column = this.state.column;
-    const numberIds = Array.from(column.numberIds);
-    numberIds.splice(source.index, 1);
-    numberIds.splice(destination.index, 0, draggableId);
-
-    this.setState({
-      column: {
-        id: this.state.column.id,
-        numberIds: Object.keys(this.state.numbers)
-      }
-    })
-    */
+    this.setState(columns);
   }
 
   render() {
-    const column = {
+    const column1 = {
       id: 'column-1',
       numbers: this.state.columns['column-1']
     }
-    console.log(column);
+    const column2 = {
+      id: 'column-2',
+      numbers: this.state.columns['column-2']
+    }
 
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <Row column={column}
-             items={column.numbers}>
+      <DragDropContext className="Board"
+                       onDragEnd={this.onDragEnd}>
+        <Row column={column1}
+             items={column1.numbers}>
+        </Row>
+        <Row column={column2}
+             items={column2.numbers}>
         </Row>
       </DragDropContext>
     )
