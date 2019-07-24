10b27f5dde6a52c2a63c222600bde3a592f18914
Cooper Barth <cooperfbarth@gmail.com>
2019-07-23 22:46:58 -0500

    Search Bar

--- src/components/MainPage/Sidebar.js
+++ src/components/MainPage/Sidebar.js
@@ -18,13 +18,12 @@ class Sidebar extends Component {
     }
 
     render() {
-        console.log(this.props.column);
         const search = this.state.searchValue.toLowerCase();
         return (
             <div className="Sidebar">
                 <SearchBar onChange={e => this.onChange(e)}
                            value={this.state.searchValue}/>
-                <SearchBody column={this.props.column.filter(block => search? block.content.toLowerCase().includes(search) : false)}/>
+                <SearchBody column={this.props.column.filter(block => (search? block.content.toLowerCase().includes(search) : false))}/>
             </div>
         )
     }
