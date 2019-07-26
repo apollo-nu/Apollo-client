e559fb91c41a1a73f18bfaad1b487abcce45a6db
Cooper Barth <cooperfbarth@gmail.com>
2019-07-26 13:04:19 -0500

    Added board and column titles

--- src/components/MainPage/Column.js
+++ src/components/MainPage/Column.js
@@ -12,21 +12,24 @@ class Column extends Component {
 
     render() {
         return (
-            <Droppable droppableId={this.props.id}>
-                {provided => (
-                    <div className="Column"
-                         style={this.props.style}
-                         ref={provided.innerRef}
-                         {...provided.droppableProps}
-                         {...provided.droppablePlaceholder}>
-                         {this.props.items.slice(0, MAX_CARDS).map(block => <Card key={block._id}
-                                                                                  draggableId={block._id}
-                                                                                  index={this.props.items.indexOf(block)}
-                                                                                  title={this.displayString(block.course)}/>)}
-                         {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
+            <div className="ColumnWrapper">
+                Column Title
+                <Droppable droppableId={this.props.id}>
+                    {provided => (
+                        <div className="Column"
+                            style={this.props.style}
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                            {...provided.droppablePlaceholder}>
+                            {this.props.items.slice(0, MAX_CARDS).map(block => <Card key={block._id}
+                                                                                    draggableId={block._id}
+                                                                                    index={this.props.items.indexOf(block)}
+                                                                                    title={this.displayString(block.course)}/>)}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </div>
         );
     }
 }
