7753aa87b9a47d7a33d60a4d359bb7363d2f13a9
Cooper Barth <cooperfbarth@gmail.com>
2019-07-26 12:00:13 -0500

    Added grid formatting to columns

--- src/components/MainPage/Board.js
+++ src/components/MainPage/Board.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Column from "./Column";
 
+const COLUMN_COUNT = 4;
+
 class Board extends Component {
     render() {
         const columns = this.props.columns;
+        const keys = Object.keys(columns);
         return (
             <div className="Board">
-                {Object.keys(columns).map((key, i) => <Column key={i}
-                                                           id={key}
-                                                           items={columns[key]}/>)}
+                {keys.map((key, i) => <Column key={i}
+                                              id={key}
+                                              items={columns[key]}
+                                              style={{
+                                                "grid-column-start": keys.indexOf(key) % COLUMN_COUNT,
+                                                "grid-row-start": Math.floor(keys.indexOf(key) / COLUMN_COUNT)
+                                              }}/>)}
             </div>
         );
     }
