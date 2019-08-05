fed2be7f1d874e6120b096684418d44c38058c06
Cooper Barth <cooper.barth@getbraintree.com>
2019-08-05 12:59:40 -0500

    Added simple css for dropdown items:

--- src/components/MainPage/Dropdown.js
+++ src/components/MainPage/Dropdown.js
@@ -2,13 +2,33 @@ import React, { Component } from "react";
 import MaterialIcon from "material-icons-react";
 
 class Dropdown extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dropdownVisible: true,
+            listTitles: [
+                "Edit Years",
+                "Log Out"
+            ]
+        };
+    }
+
     render() {
         return (
-            <div className="Dropdown">
-                <MaterialIcon icon="reorder"
-                              size="50"
-                              invert/>
-            </div>
+            <React.Fragment>
+                <div className="Dropdown">
+                    <MaterialIcon icon="reorder"
+                                size="50"
+                                invert/>
+                </div>
+                {this.state.dropdownVisible?
+                    <div className="DropdownList">
+                        {this.state.listTitles.map(title => (
+                            <div className="DropdownListItem">{title}</div>
+                        ))}
+                    </div>
+                :null}
+            </React.Fragment>
         )
     }
 }
